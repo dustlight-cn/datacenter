1fa51dfa70d845982b84c2af1b1a743b048ae837
Hansin1997 <845612500@qq.com>
2021-07-20 21:51:25 +0800

    [Update] FormRecordSearcher

--- datacenter-elasticsearch/src/main/java/plus/datacenter/elasticsearch/services/ElasticsearchFormRecordSearcher.java
+++ datacenter-elasticsearch/src/main/java/plus/datacenter/elasticsearch/services/ElasticsearchFormRecordSearcher.java
@@ -3,7 +3,7 @@ package plus.datacenter.elasticsearch.services;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
@@ -11,6 +11,7 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import plus.auth.entities.QueryResult;
 import plus.datacenter.core.entities.forms.FormRecord;
 import plus.datacenter.core.entities.queries.Query;
+import plus.datacenter.core.entities.queries.queries.BetweenQuery;
 import plus.datacenter.core.services.FormRecordSearcher;
 import reactor.core.publisher.Mono;
 
@@ -35,7 +36,12 @@ public class ElasticsearchFormRecordSearcher implements FormRecordSearcher {
         IndexCoordinates index = IndexCoordinates.of(indexPrefix + "." + clientId + "." + formName + ".*");
         BoolQueryBuilder bq = new BoolQueryBuilder();
 
-
+        if (queries != null) {
+            for (Query query : queries) {
+                QueryBuilder queryBuilder = transform(query);
+                bq.must(queryBuilder);
+            }
+        }
 
         NativeSearchQuery nqb = new NativeSearchQuery(bq).setPageable(Pageable.ofSize(size).withPage(page));
         return operations.searchForPage(nqb,
@@ -44,5 +50,35 @@ public class ElasticsearchFormRecordSearcher implements FormRecordSearcher {
                 .map(searchHits -> new QueryResult((int) searchHits.getTotalElements(),
                         searchHits.getContent().stream().map(formSearchHit ->
                                 formSearchHit.getContent()).collect(Collectors.toList())));
+    }
+
+    protected QueryBuilder transform(Query query) {
+        switch (query.getOpt()) {
+            case MATCH:
+                return new MatchQueryBuilder(query.getName(), query.getValue());
+            case BETWEEN:
+                RangeQueryBuilder rq = new RangeQueryBuilder(query.getName());
+                BetweenQuery bq = (BetweenQuery) query;
+                if (bq.getValue() != null) {
+                    if (bq.getValue().isOpenInterval()) {
+                        if (bq.getValue().getMin() != null)
+                            rq.gt(bq.getValue().getMin());
+                        if (bq.getValue().getMax() != null)
+                            rq.lt(bq.getValue().getMax());
+                    } else {
+                        if (bq.getValue().getMin() != null)
+                            rq.gte(bq.getValue().getMin());
+                        if (bq.getValue().getMax() != null)
+                            rq.lte(bq.getValue().getMax());
+                    }
+                }
+
+                return rq;
+            case IN:
+                return new TermsQueryBuilder(query.getName(), (Collection) query.getValue());
+            case EQUAL:
+            default:
+                return new TermQueryBuilder(query.getName(), query.getValue());
+        }
     }
 }

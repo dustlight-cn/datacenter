25d7c982d66696a22bdb29fecf09144711edf2b9
Hansin1997 <845612500@qq.com>
2021-07-30 19:31:58 +0800

    fix(ElasticsearchRecordService): 修复更新记录时，updated 变成 long 的问题。

--- datacenter-elasticsearch/src/main/java/plus/datacenter/elasticsearch/services/ElasticsearchFormRecordService.java
+++ datacenter-elasticsearch/src/main/java/plus/datacenter/elasticsearch/services/ElasticsearchFormRecordService.java
@@ -48,7 +48,7 @@ public class ElasticsearchFormRecordService implements FormRecordService {
         IndexCoordinates index = IndexCoordinates.of(indexPrefix + "." + target.getClientId() + "." + target.getFormName() + "." + target.getFormId());
         Map<String, Object> update = new HashMap<>();
         Map<String, Object> data = new HashMap<>();
-        update.put("updatedAt", target.getUpdatedAt().toEpochMilli());
+        update.put("updatedAt", target.getUpdatedAt());
         update.put("data", data);
         Iterator<Map.Entry<String, Object>> iterator = target.getData().entrySet().iterator();
         while (iterator.hasNext()) {

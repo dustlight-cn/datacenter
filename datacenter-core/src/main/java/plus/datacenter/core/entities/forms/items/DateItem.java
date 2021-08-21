2565ef3127d78e7f1c677b7cb68860e735c6213c
Hansin1997 <845612500@qq.com>
2021-08-21 18:09:18 +0800

    Es2Mongo —— 从 Elasticsearch 还原 Mongo 数据。

--- datacenter-core/src/main/java/plus/datacenter/core/entities/forms/items/DateItem.java
+++ datacenter-core/src/main/java/plus/datacenter/core/entities/forms/items/DateItem.java
@@ -1,25 +1,19 @@
 package plus.datacenter.core.entities.forms.items;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-import plus.datacenter.core.entities.Validatable;
+import plus.datacenter.core.entities.Rangeable;
 import plus.datacenter.core.entities.forms.Item;
 import plus.datacenter.core.entities.forms.ItemType;
 
-import java.io.Serializable;
 import java.time.Instant;
 
 @Getter
 @Setter
 public class DateItem extends Item<Instant> {
 
-    private InstantRangeable dateRange;
+    private Rangeable<Instant> dateRange;
 
     @Override
     public Boolean validate(Instant value) {
@@ -37,33 +31,6 @@ public class DateItem extends Item<Instant> {
     @Override
     public void setType(ItemType type) {
         super.setType(ItemType.DATE);
-    }
-
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public class InstantRangeable implements Validatable<Instant>, Serializable {
-
-        @Field(type = FieldType.Date, format = DateFormat.date_optional_time)
-        private Instant min;
-
-        @Field(type = FieldType.Date, format = DateFormat.date_optional_time)
-        private Instant max;
-
-        private boolean openInterval;
-
-        @Override
-        public Boolean validate(Instant value) {
-            if (value == null)
-                return min == null && max == null;
-            if (min != null && (openInterval ? (value.compareTo(min) < 0) : (value.compareTo(min) <= 0)))
-                return false;
-            if (max != null && (openInterval ? (value.compareTo(max) > 0) : (value.compareTo(max) >= 0)))
-                return false;
-            return true;
-        }
     }
 
 }
---
+++ tools/es2mongo/src/main/java/plus/datacenter/es2mongo/FormMeta.java
@@ -0,0 +1,17 @@
+package plus.datacenter.es2mongo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+public class FormMeta {
+
+    @Id
+    private String name;
+    private String clientId;
+    private String owner;
+    private String currentId;
+    private int version = 0;
+}

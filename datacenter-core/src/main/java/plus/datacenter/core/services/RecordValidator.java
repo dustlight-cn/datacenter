91852edd8abfdbfdcf903e7d122d64269a35755d
Hansin1997 <845612500@qq.com>
2021-09-08 13:44:10 +0800

    准备实现 RecordValidator。

--- datacenter-core/src/main/java/plus/datacenter/core/services/RecordValidator.java
+++ datacenter-core/src/main/java/plus/datacenter/core/services/RecordValidator.java
@@ -1,4 +1,25 @@
 package plus.datacenter.core.services;
 
+import plus.datacenter.core.entities.DatacenterPrincipal;
+import plus.datacenter.core.entities.forms.Record;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+
+/**
+ * 记录验证器
+ */
 public interface RecordValidator {
+
+    /**
+     * 验证记录
+     *
+     * @param records   待验证记录集合
+     * @param principal 当前用户身份
+     * @param clientId  应用ID
+     * @return 校验完成后的记录集合
+     */
+    Mono<Collection<Record>> validate(Collection<Record> records,
+                                      DatacenterPrincipal principal,
+                                      String clientId);
 }
